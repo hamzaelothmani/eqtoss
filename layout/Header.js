2283f3de713198780b7ff2ad9fa5149c4f833016
hamzaelothmani <105682345+hamzaelothmani@users.noreply.github.com>
2022-10-11 22:54:09 +0100

    navbar
    100%

--- layout/Header.js
+++ layout/Header.js
@@ -2,23 +2,312 @@ import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { signOut } from "next-auth/react"
+import { signOut } from "next-auth/react";
 import { motion } from "framer-motion";
+import { MdOutlineBookmarkAdd } from "react-icons/md";
 
 export default function Header() {
-  const [navbar, setNavbar] = useState(false);
+  // const [navbar, setNavbar] = useState(false);
+  const [click, setClick] = useState(false);
 
+  const [profile, setProfile] = useState(false);
 
   const router = useRouter();
 
   const { data: session, status } = useSession();
-  // console.log(session);
-
- 
+  console.log(session?.user.image);
 
   return (
     <>
-      <nav id="head" className="w-full  bg-white shadow sticky top-0 z-50">
+      <nav class="bg-white shadow">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="flex justify-between h-16">
+            <div class="flex">
+              <div class="flex-shrink-0 flex items-center">
+                <img
+                  class="block lg:hidden h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt="Workflow"
+                />
+                <img
+                  class="hidden lg:block h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                  alt="Workflow"
+                />
+              </div>
+              <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
+                <Link href="/">
+                  <a class="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                    {" "}
+                    Home
+                  </a>
+                </Link>
+                <Link href="/ShoppingList">
+                  <a class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                    {" "}
+                    Deals{" "}
+                  </a>
+                </Link>
+                <Link href="/PostForm">
+                  <a class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                    {" "}
+                    Poster{" "}
+                  </a>
+                </Link>
+                <Link href="">
+                  <a class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+                    {" "}
+                    About{" "}
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <div class="hidden sm:ml-6 sm:flex sm:items-center">
+              <button
+                type="button"
+                class="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span class="sr-only">View notifications</span>
+
+                <MdOutlineBookmarkAdd className="w-6 h-6" />
+              </button>
+
+              <div class="ml-3 relative">
+                <div>
+                  <button
+                    onClick={() => setProfile(!profile)}
+                    type="button"
+                    class="bg-white rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    aria-haspopup="true"
+                  >
+                    <span class="sr-only">Open user menu</span>
+                    <div class="h-8 w-8 bg-black rounded-full"></div>
+                    {/* <img class="h-8 w-8 rounded-full" src={session.user.image} /> */}
+                  </button>
+                </div>
+
+                {profile && (
+                  <div
+                    class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="user-menu-button"
+                    tabindex="-1"
+                  >
+                    {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700"
+                      role="menuitem"
+                      tabindex="-1"
+                      id="user-menu-item-0"
+                    >
+                      Your Profile
+                    </a>
+                    <Link href="/Register">
+                      <a
+                        class={`${
+                          router.asPath == "/Register" ? "hidden" : "block"
+                        } px-4 py-2 text-sm text-gray-700`}
+                        role="menuitem"
+                        tabindex="-1"
+                        id="user-menu-item-1"
+                      >
+                        Register
+                      </a>
+                    </Link>
+                    {status === "authenticated" ? (
+                      <a
+                        onClick={() => signOut()}
+                        class="block px-4 py-2 text-sm text-gray-700"
+                        role="menuitem"
+                        tabindex="-1"
+                        id="user-menu-item-2"
+                      >
+                        Sign Out
+                      </a>
+                    ) : (
+                      <Link href="/Login">
+                        <a
+                          class={`${
+                            router.asPath == "/Login" ? "hidden" : "block"
+                          } px-4 py-2 text-sm text-gray-700`}
+                          role="menuitem"
+                          tabindex="-1"
+                          id="user-menu-item-2"
+                        >
+                          Sign In
+                        </a>
+                      </Link>
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div class="-mr-2 flex items-center sm:hidden">
+              <button
+                onClick={() => setClick(!click)}
+                type="button"
+                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span class="sr-only">Open main menu</span>
+
+                <svg
+                  class="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+
+                <svg
+                  class="hidden h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {click && (
+          <div class="sm:hidden" id="mobile-menu">
+            <div class="pt-2 pb-3 space-y-1">
+              {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
+              <Link href="/">
+                <a
+                  href="#"
+                  class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Home
+                </a>
+              </Link>
+              <Link href="/ShoppingList">
+                <a
+                  href="#"
+                  class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Deals
+                </a>
+              </Link>
+              <Link href="PostForm">
+                <a
+                  href="#"
+                  class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  Poster
+                </a>
+              </Link>
+              <Link href="/">
+                <a
+                  href="#"
+                  class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                >
+                  About
+                </a>
+              </Link>
+            </div>
+            <div class="pt-4 pb-3 border-t border-gray-200">
+              <div class="flex items-center px-4">
+                <div class="flex-shrink-0">
+                  <img
+                    class="h-10 w-10 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+                <div class="ml-3">
+                  <div class="text-base font-medium text-gray-800">
+                    {" "}
+                    {session?.user.name}{" "}
+                  </div>
+                  <div class="text-sm font-medium text-gray-500">
+                    {session?.user.email}{" "}
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  class="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <span class="sr-only">View notifications</span>
+
+                  <MdOutlineBookmarkAdd className="w-6 h-6" />
+                </button>
+              </div>
+              <div class="mt-3 space-y-1">
+                <a
+                  href="#"
+                  class="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="user-menu-item-0"
+                >
+                  Your Profile
+                </a>
+                <Link href="/Register">
+                  <a
+                    class={`${
+                      router.asPath == "/Register" ? "hidden" : "block"
+                    } px-4 py-2 text-sm text-gray-700`}
+                    role="menuitem"
+                    tabindex="-1"
+                    id="user-menu-item-1"
+                  >
+                    Register
+                  </a>
+                </Link>
+                {status === "authenticated" ? (
+                  <a
+                    onClick={() => signOut()}
+                    class="block px-4 py-2 text-sm text-gray-700"
+                    role="menuitem"
+                    tabindex="-1"
+                    id="user-menu-item-2"
+                  >
+                    Sign Out
+                  </a>
+                ) : (
+                  <Link href="/Login">
+                    <a
+                      class={`${
+                        router.asPath == "/Login" ? "hidden" : "block"
+                      } px-4 py-2 text-sm text-gray-700`}
+                      role="menuitem"
+                      tabindex="-1"
+                      id="user-menu-item-2"
+                    >
+                      Sign In
+                    </a>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
+      </nav>
+      {/* <nav id="head" className="w-full  bg-white shadow sticky top-0 z-50">
         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -120,8 +409,7 @@ export default function Header() {
             </div>
           </div>
         </div>
-      </nav>
+      </nav> */}
     </>
   );
 }
-
